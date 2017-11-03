21e4acbec2dc9b2811afeedc1401ee7365cc8d6f
suhaya3317 <hayato_17n1200071@nnn.ed.jp>
2017-11-03 16:55:42 +0900

    go fmt index_test.go

--- index_test.go
+++ index_test.go
@@ -22,23 +22,23 @@ func Testancestor(t *testing.T) {
 	}
 }
 
-func Testmain(t *testing.T)  {
-  stdin := bufio.NewScanner(os.Stdin)
+func Testmain(t *testing.T) {
+	stdin := bufio.NewScanner(os.Stdin)
 	stdin.Scan()
 	text := stdin.Text()
-  if text == "ファッション" {
-    fmt.Println("子孫カテゴリは「ファッション」です")
-  } else if text == "レディース" {
-    fmt.Println("子孫カテゴリは「レディース／靴」です")
-  } else if text == "メンズ" {
-    fmt.Println("子孫カテゴリは「メンズ」です")
-  } else if text == "靴" {
-    fmt.Println("子孫カテゴリは「靴」です")
-  } else if text == "家具・インテリア" {
-    fmt.Println("子孫カテゴリは「家具・インテリア／収納家具」です")
-  } else if text == "収納家具" {
-    fmt.Println("子孫カテゴリは「収納家具」です")
-  } else {
-    t.Error("failed")
-  }
+	if text == "ファッション" {
+		fmt.Println("子孫カテゴリは「ファッション」です")
+	} else if text == "レディース" {
+		fmt.Println("子孫カテゴリは「レディース／靴」です")
+	} else if text == "メンズ" {
+		fmt.Println("子孫カテゴリは「メンズ」です")
+	} else if text == "靴" {
+		fmt.Println("子孫カテゴリは「靴」です")
+	} else if text == "家具・インテリア" {
+		fmt.Println("子孫カテゴリは「家具・インテリア／収納家具」です")
+	} else if text == "収納家具" {
+		fmt.Println("子孫カテゴリは「収納家具」です")
+	} else {
+		t.Error("failed")
+	}
 }
