6576ef1f9f6c69e7f99d96cf4f7c8ba79c1ca5cb
suhaya3317 <hayato_17n1200071@nnn.ed.jp>
2017-11-03 15:45:19 +0900

    Made index_text.go

---
+++ index_test.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"testing"
+)
+
+func Testancestor(t *testing.T) {
+	fmt.Println("カテゴリ名を入力してください")
+
+	stdin := bufio.NewScanner(os.Stdin)
+	stdin.Scan()
+	text := stdin.Text()
+
+	if text == "ファッション" || text == "家具・インテリア" {
+		fmt.Println("先祖カテゴリです")
+	} else {
+		t.Error("failed")
+	}
+}
+
+func Testmain(t *testing.T)  {
+  stdin := bufio.NewScanner(os.Stdin)
+	stdin.Scan()
+	text := stdin.Text()
+  if text == "ファッション" {
+    fmt.Println("子孫カテゴリは「ファッション」です")
+  } else if text == "レディース" {
+    fmt.Println("子孫カテゴリは「レディース／靴」です")
+  } else if text == "メンズ" {
+    fmt.Println("子孫カテゴリは「メンズ」です")
+  } else if text == "靴" {
+    fmt.Println("子孫カテゴリは「靴」です")
+  } else if text == "家具・インテリア" {
+    fmt.Println("子孫カテゴリは「家具・インテリア／収納家具」です")
+  } else if text == "収納家具" {
+    fmt.Println("子孫カテゴリは「収納家具」です")
+  } else {
+    t.Error("failed")
+  }
+}
